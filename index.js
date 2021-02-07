ab836c4246eda897f4319968482f80b5dd2d1808
jwilson42 <wilson.jd022@gmail.com>
2021-02-07 11:55:15 -0800

    forgot to commit updates to app

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ const expressEjsLayouts = require('express-ejs-layouts');
 // PUT and DELETE
 const methodOverride = require('method-override');
 // Axios (if you want it)
-const axios = require('axios');
+// const axios = require('axios');
+// Database
+const db = require('./models');
 
 // MIDDLEWARE
 
@@ -29,7 +31,43 @@ app.use(require('morgan')('dev'));
 // ROUTES or Controlers
 
 app.get('/', (req, res) => {
-    res.send("I'm working");
+    res.redirect('/coders');
+});
+
+// The Route to get all coders
+app.get('/coders', (req, res) => {
+    db.user.findAll()
+    .then(users => {
+        res.render('./index', { users });
+    }).catch(error => {
+        console.log("An error has occured: ", error);
+    });
+});
+
+// To the new coder form
+app.get('/coders/new', (req, res) => {
+    res.render('./new');
+});
+
+// POST
+
+// Add a new coder to the database
+app.post('/coders', (req, res) => {
+
+    // Something weird here, it will actually parse strings into their own
+    // datatypes, but in reality, you should be parsing them yourself just in case.
+    try {
+        db.user.create(req.body)
+        .then(data => {
+            console.log('New user created');
+            res.redirect('/coders');
+        });
+    } catch (error) {
+        console.log('An error has occured');
+        res.status(401);
+        res.redirect('/coders');
+    }
+
 });
 
 const PORT = process.env.PORT || 8000;
